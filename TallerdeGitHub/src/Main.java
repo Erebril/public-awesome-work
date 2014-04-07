1bd5c8091316430efc3312dbdf29aa6f739f135c
Erebril <pablor.0@gmail.com>
2014-04-07 17:06:47 -0300

    agregada otra linea

--- TallerdeGitHub/src/Main.java
+++ TallerdeGitHub/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		    System.out.println("Hola Mundo Git!");
-		    
+		    System.out.println("es muy complicado y nos va a generar retrasos... yei!");
 
 	}
 
